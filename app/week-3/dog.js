a313066140716855f9aafce8f8d88fbd6abeb03d
Kshitij Tagra <130739500+kshitij-tagra@users.noreply.github.com>
2024-01-22 12:47:39 -0700

    Complete Week-3 Demo

---
+++ app/week-3/dog.js
@@ -0,0 +1,10 @@
+export default function Dog({name, breed, age, color}) {
+  return (
+    <div className="ml-4">
+      <h2 className="text-green-500">{name}</h2>
+      <p className="text-xs">
+        {breed}, {age} years old, {color}
+      </p>
+    </div>
+  );
+}
---
+++ app/week-3/page.js
@@ -0,0 +1,49 @@
+import Dog from "./dog";
+
+export default function Page() {
+  let dog1 = {
+    name: "Fluffy",
+    breed: "Poodle",
+    age: 4,
+    color: "white",
+  };
+
+  let dog2 = {
+    name: "Spot",
+    breed: "Dalmatian",
+    age: 7,
+    color: "brown",
+  };
+
+  let dog3 = {
+    name: "Rover",
+    breed: "Golden Retriever",
+    age: 2,
+    color: "golden",
+  };
+
+  return (
+    <main className="m-4 ml-6">
+      <h1 className="text-4xl font-bold">Week 3</h1>
+      <p>These are my favourite dogs.</p>
+      <Dog
+        name={dog1.name}
+        breed={dog1.breed}
+        age={dog1.age}
+        color={dog1.color}
+      />
+      <Dog
+        name={dog2.name}
+        breed={dog2.breed}
+        age={dog2.age}
+        color={dog2.color}
+      />
+      <Dog
+        name={dog3.name}
+        breed={dog3.breed}
+        age={dog3.age}
+        color={dog3.color}
+      />
+    </main>
+  );
+}
